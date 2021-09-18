b549e8740db83d5fb16d742d2c0fad061c67e3a3
KratosV1 <90736066+KratosV1@users.noreply.github.com>
2021-09-17 21:07:50 -0500

    Update menuarteuwu.js

--- basededatosxd/panelmenu/menuarteuwu.js
+++ basededatosxd/panelmenu/menuarteuwu.js
@@ -2,111 +2,111 @@ const menuarteuwu = (prefix, sender) => {
         return `
 𝐂𝐫𝐞𝐚𝐜𝐢𝐨𝐧 𝐝𝐞 𝐥𝐨𝐠𝐨𝐬
 ╔══════════════╗
-║➩ ❍ ${prefix}videtexto
-║➩ ❍ ${prefix}creavideoxd
-║➩ ❍ ${prefix}logocosmic
-║➩ ❍ ${prefix}fraseedit
-║➩ ❍ ${prefix}shadow
-║➩ ❍ ${prefix}gneon
-║➩ ❍ ${prefix}matrixlogo
-║➩ ❍ ${prefix}cup
-║➩ ❍ ${prefix}cup1
-║➩ ❍ ${prefix}romance
-║➩ ❍ ${prefix}smoke
-║➩ ❍ ${prefix}burnpaper
-║➩ ❍ ${prefix}lovemessage
-║➩ ❍ ${prefix}undergrass
-║➩ ❍ ${prefix}love
-║➩ ❍ ${prefix}coffe
-║➩ ❍ ${prefix}woodheart
-║➩ ❍ ${prefix}woodenboard
-║➩ ❍ ${prefix}summer3d
-║➩ ❍ ${prefix}wolfmetal
-║➩ ❍ ${prefix}nature3d
-║➩ ❍ ${prefix}underwater
-║➩ ❍ ${prefix}golderrose
-║➩ ❍ ${prefix}summernature
-║➩ ❍ ${prefix}letterleaves
-║➩ ❍ ${prefix}glowingneon
-║➩ ❍ ${prefix}fallleaves
-║➩ ❍ ${prefix}flamming
-║➩ ❍ ${prefix}harrypotter
-║➩ ❍ ${prefix}carvedwood
-║➩ ❍ ${prefix}blackpink
-║➩ ❍ ${prefix}neon
-║➩ ❍ ${prefix}greenneon
-║➩ ❍ ${prefix}advanceglow
-║➩ ❍ ${prefix}futureneon
-║➩ ❍ ${prefix}sandwriting
-║➩ ❍ ${prefix}sandsummer
-║➩ ❍ ${prefix}sandengraved
-║➩ ❍ ${prefix}metaldark
-║➩ ❍ ${prefix}neonlight
-║➩ ❍ ${prefix}holographic
-║➩ ❍ ${prefix}text1917
-║➩ ❍ ${prefix}minion
-║➩ ❍ ${prefix}deluxesilver
-║➩ ❍ ${prefix}newyearcard
-║➩ ❍ ${prefix}bloodfrosted
-║➩ ❍ ${prefix}halloween
-║➩ ❍ ${prefix}jokerlogo
-║➩ ❍ ${prefix}fireworksparkle
-║➩ ❍ ${prefix}natureleaves
-║➩ ❍ ${prefix}bokeh
-║➩ ❍ ${prefix}toxic
-║➩ ❍ ${prefix}strawberry
-║➩ ❍ ${prefix}box3d
-║➩ ❍ ${prefix}roadwarning
-║➩ ❍ ${prefix}breakwall
-║➩ ❍ ${prefix}icecold
-║➩ ❍ ${prefix}luxury
-║➩ ❍ ${prefix}cloud
-║➩ ❍ ${prefix}summersand
-║➩ ❍ ${prefix}horrorblood
-║➩ ❍ ${prefix}thunder
-║➩ ❍ ${prefix}bannerlol
-║➩ ❍ ${prefix}arcade8bit
-║➩ ❍ ${prefix}writegalacy
-║➩ ❍ ${prefix}textbyname
-║➩ ❍ ${prefix}galaxybat
-║➩ ❍ ${prefix}snow3d
-║➩ ❍ ${prefix}birthdayday
-║➩ ❍ ${prefix}goldplaybutton
-║➩ ❍ ${prefix}silverplaybutton
-║➩ ❍ ${prefix}wallgravity
-║➩ ❍ ${prefix}steel3d
-║➩ ❍ ${prefix}wolflogo
-║➩ ❍ ${prefix}lionlogo
-║➩ ❍ ${prefix}ninjalogo
-║➩ ❍ ${prefix}marvelstudio
-║➩ ❍ ${prefix}space
-║➩ ❍ ${prefix}avenger
-║➩ ❍ ${prefix}wooden3d
-║➩ ❍ ${prefix}starsnight
-║➩ ❍ ${prefix}textcake
-║➩ ❍ ${prefix}glittergold
-║➩ ❍ ${prefix}metallogo
-║➩ ❍ ${prefix}noeltext
-║➩ ❍ ${prefix}greenbush
-║➩ ❍ ${prefix}glossychrome
-║➩ ❍ ${prefix}greenneon
-║➩ ❍ ${prefix}hologram3d
-║➩ ❍ ${prefix}galaxystyle
-║➩ ❍ ${prefix}birthdaycake
-║➩ ❍ ${prefix}heartshaped
-║➩ ❍ ${prefix}royaltext
-║➩ ❍ ${prefix}puppycute
-║➩ ❍ ${prefix}beautifulflower
-║➩ ❍ ${prefix}lighttext
-║➩ ❍ ${prefix}galaxywallpaper
-║➩ ❍ ${prefix}luxurygold
-║➩ ❍ ${prefix}watercolor
-║➩ ❍ ${prefix}multicolor3d
-║➩ ❍ ${prefix}wetglass
-║➩ ❍ ${prefix}pubg
-║➩ ❍ ${prefix}battlefield4
-║➩ ❍ ${prefix}pornhub
-║➩ ❍ ${prefix}glitch
+║ ${prefix}videtexto
+║ ${prefix}creavideoxd
+║ ${prefix}logocosmic
+║ ${prefix}fraseedit
+║ ${prefix}shadow
+║ ${prefix}gneon
+║ ${prefix}matrixlogo
+║ ${prefix}cup
+║ ${prefix}cup1
+║ ${prefix}romance
+║ ${prefix}smoke
+║ ${prefix}burnpaper
+║ ${prefix}lovemessage
+║ ${prefix}undergrass
+║ ${prefix}love
+║ ${prefix}coffe
+║ ${prefix}woodheart
+║ ${prefix}woodenboard
+║ ${prefix}summer3d
+║ ${prefix}wolfmetal
+║ ${prefix}nature3d
+║ ${prefix}underwater
+║ ${prefix}golderrose
+║ ${prefix}summernature
+║ ${prefix}letterleaves
+║ ${prefix}glowingneon
+║ ${prefix}fallleaves
+║ ${prefix}flamming
+║ ${prefix}harrypotter
+║ ${prefix}carvedwood
+║ ${prefix}blackpink
+║ ${prefix}neon
+║ ${prefix}greenneon
+║ ${prefix}advanceglow
+║ ${prefix}futureneon
+║ ${prefix}sandwriting
+║ ${prefix}sandsummer
+║ ${prefix}sandengraved
+║ ${prefix}metaldark
+║ ${prefix}neonlight
+║ ${prefix}holographic
+║ ${prefix}text1917
+║ ${prefix}minion
+║ ${prefix}deluxesilver
+║ ${prefix}newyearcard
+║ ${prefix}bloodfrosted
+║ ${prefix}halloween
+║ ${prefix}jokerlogo
+║ ${prefix}fireworksparkle
+║ ${prefix}natureleaves
+║ ${prefix}bokeh
+║ ${prefix}toxic
+║ ${prefix}strawberry
+║ ${prefix}box3d
+║ ${prefix}roadwarning
+║ ${prefix}breakwall
+║ ${prefix}icecold
+║ ${prefix}luxury
+║ ${prefix}cloud
+║ ${prefix}summersand
+║ ${prefix}horrorblood
+║ ${prefix}thunder
+║ ${prefix}bannerlol
+║ ${prefix}arcade8bit
+║ ${prefix}writegalacy
+║ ${prefix}textbyname
+║ ${prefix}galaxybat
+║ ${prefix}snow3d
+║ ${prefix}birthdayday
+║ ${prefix}goldplaybutton
+║ ${prefix}silverplaybutton
+║ ${prefix}wallgravity
+║ ${prefix}steel3d
+║ ${prefix}wolflogo
+║ ${prefix}lionlogo
+║ ${prefix}ninjalogo
+║ ${prefix}marvelstudio
+║ ${prefix}space
+║ ${prefix}avenger
+║ ${prefix}wooden3d
+║ ${prefix}starsnight
+║ ${prefix}textcake
+║ ${prefix}glittergold
+║ ${prefix}metallogo
+║ ${prefix}noeltext
+║ ${prefix}greenbush
+║ ${prefix}glossychrome
+║ ${prefix}greenneon
+║ ${prefix}hologram3d
+║ ${prefix}galaxystyle
+║ ${prefix}birthdaycake
+║ ${prefix}heartshaped
+║ ${prefix}royaltext
+║ ${prefix}puppycute
+║ ${prefix}beautifulflower
+║ ${prefix}lighttext
+║ ${prefix}galaxywallpaper
+║ ${prefix}luxurygold
+║ ${prefix}watercolor
+║ ${prefix}multicolor3d
+║ ${prefix}wetglass
+║ ${prefix}pubg
+║ ${prefix}battlefield4
+║ ${prefix}pornhub
+║ ${prefix}glitch
 ╚══════════════╝
 𝙿𝚘𝚛 𝚎𝚕 𝚕𝚒𝚖𝚒𝚝𝚊𝚍𝚘 𝚙𝚛𝚘𝚌𝚎𝚜𝚊𝚖𝚒𝚎𝚗𝚝𝚘 𝚍𝚎 𝚕𝚘𝚜 𝚖𝚘𝚟𝚒𝚕𝚎𝚜, 𝚙𝚞𝚎𝚍𝚎 𝚚𝚞𝚎 𝚊𝚕𝚐𝚞𝚗𝚊𝚜 𝚒𝚖𝚊𝚐𝚎𝚗𝚎𝚜 𝚎𝚍𝚒𝚝𝚊𝚍𝚊𝚜 𝚘 𝚟𝚒𝚍𝚎𝚘𝚜 𝚗𝚘 𝚜𝚎𝚊𝚗 𝚙𝚛𝚘𝚌𝚎𝚜𝚊𝚍𝚊𝚜 𝚍𝚎 𝚖𝚊𝚗𝚎𝚛𝚊 𝚌𝚘𝚛𝚛𝚎𝚌𝚝𝚊 𝚢 𝚗𝚘 𝚛𝚎𝚌𝚒𝚋𝚊𝚗 𝚗𝚊𝚍𝚊 𝚍𝚎 𝚋𝚘𝚝
   
